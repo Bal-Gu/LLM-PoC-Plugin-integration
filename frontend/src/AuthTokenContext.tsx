507147449f569917025c9c0d8644bfa2ebb3c9df
BalGu <guillaume@ballinger.lu>
2024-03-30 01:44:56 +0100

    modifing the auth token passing

--- frontend/src/AuthTokenContext.tsx
+++ frontend/src/AuthTokenContext.tsx
@@ -1,3 +1,5 @@
-import React from 'react';
+import { createContext, Dispatch, SetStateAction } from 'react';
 
-export const AuthTokenContext = React.createContext<string | null>(null);
+type AuthTokenContextType = [string | null, Dispatch<SetStateAction<string | null>> | null];
+
+export const AuthTokenContext = createContext<AuthTokenContextType>([null, null]);

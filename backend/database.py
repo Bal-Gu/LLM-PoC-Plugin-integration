602552cd73ad38a8db3a2d63de456aa158b289f1
BalGu <guillaume@ballinger.lu>
2024-05-02 20:13:24 +0200

    3NF

--- backend/database.py
+++ backend/database.py
@@ -25,7 +25,11 @@ class Database:
 
         # User table
         self.mycursor.execute(
-            "CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255),salt VARCHAR(5), auth_token VARCHAR(255), admin BOOLEAN)")
+            "CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), admin BOOLEAN)")
+
+        # Auth table
+        self.mycursor.execute(
+            "CREATE TABLE IF NOT EXISTS auth (id INT AUTO_INCREMENT PRIMARY KEY, user_id INT, password VARCHAR(255), salt VARCHAR(5), auth_token VARCHAR(255), FOREIGN KEY(user_id) REFERENCES user(id))")
 
         # Session table
         self.mycursor.execute(
@@ -35,42 +39,50 @@ class Database:
         self.mycursor.execute(
             "CREATE TABLE IF NOT EXISTS message (id INT AUTO_INCREMENT PRIMARY KEY, user_id INT, session_id INT, order_id INT, content VARCHAR(10000), FOREIGN KEY(user_id) REFERENCES user(id), FOREIGN KEY(session_id) REFERENCES session(id),status TINYINT(1))")
 
-
         self.create_user("assistant", str(random.random()), False, 1)
         self.create_user("admin", config["admin_password"], True, 2)
 
     def login(self, username, password):
+        # Fetch user data from the user table
         user = self.parallelize_and_fetch(False, "SELECT * FROM user WHERE username = %s", [username])
         if user:
-            salt = user[3]
-            db_pass = user[2]
-            hash_password = hashlib.sha3_256()
-            hash_password.update(str(username + password + salt).encode())
-            internal_pass = hash_password.hexdigest()
-            if internal_pass == db_pass:
-                return user[4]
+            # Fetch authentication data from the auth table
+            auth = self.parallelize_and_fetch(False, "SELECT * FROM auth WHERE user_id = %s", [user[0]])
+            if auth:
+                salt = auth[3]
+                db_pass = auth[2]
+                hash_password = hashlib.sha3_256()
+                hash_password.update(str(username + password + salt).encode())
+                internal_pass = hash_password.hexdigest()
+                if internal_pass == db_pass:
+                    return auth[4]
         return None
 
     def create_user(self, username, password, admin=False, user_id=-1):
         """returns True if unique"""
         user = self.parallelize_and_fetch(False, "SELECT * FROM user WHERE id = %s OR username = %s",
                                           [user_id, username])
-        auth = ''.join(random.choices(string.ascii_letters + string.digits, k=255))
+        auth_token = ''.join(random.choices(string.ascii_letters + string.digits, k=255))
         if not user:
             salt = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
             hash_password = hashlib.sha3_256()
             hash_password.update(str(username + password + salt).encode())
-            data = {
+            user_data = {
                 "username": username,
-                "password": hash_password.hexdigest(),
-                "auth_token": auth,
-                "admin": admin,
-                "salt": salt
+                "admin": admin
             }
             if user_id != -1:
-                data["id"] = user_id
-            self.save("user", data)
-            return auth
+                user_data["id"] = user_id
+            self.save("user", user_data)
+            user = self.parallelize_and_fetch(False, "SELECT * FROM user WHERE username = %s", [username])
+            auth_data = {
+                "user_id": user[0],
+                "password": hash_password.hexdigest(),
+                "auth_token": auth_token,
+                "salt": salt
+            }
+            self.save("auth", auth_data)
+            return auth_token
         return None
 
     def save(self, table: str, data: dict):
